264f5c35d6c6436a237b79323eaaaae36f7971d0
JITENDRAMINDA <49564169+JITENDRAMINDA@users.noreply.github.com>
2019-07-02 13:43:26 +0530

    Update coin.py

--- coin.py
+++ coin.py
@@ -2,7 +2,7 @@ from pyrogram import Client, Filters, Emoji
 import random
 import time
 
-app = Client("session",bot_token="663574960:AAGWg1VGruCPuckHzjbpDLRIbPWkX6YcDlc",api_id=605563,api_hash="7f2c2d12880400b88764b9b304e14e0b") 
+app = Client("session",bot_token="863961400:AAG3kaHMrOsklKBP3fGEn7T4rTyC1dXkRTc863961400:AAG3kaHMrOsklKBP3fGEn7T4rTyC1dXkRTc",api_id=605563,api_hash="7f2c2d12880400b88764b9b304e14e0b") 
 
 @app.on_message(Filters. command('toss'))
 def ran(client, message) :
