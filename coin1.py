130b604b44f21a0323979584c0ad8d79f94ac657
JITENDRAMINDA <49564169+JITENDRAMINDA@users.noreply.github.com>
2019-07-18 19:08:11 +0530

    Update coin1.py

--- coin1.py
+++ coin1.py
@@ -118,25 +118,25 @@ def ran(client, message):
   client.send_animation(message.chat.id, "https://drive.google.com/file/d/1-wmL7G1YR0_CLAHiuwgKErGtnXiBqGVq/view?usp=drivesdk") 
   x = random.choice(["1","2","3","4","5","6","7","8","9","10","5","6","4","10","3","2","1","10","5","7","8"])
   if x == "1":
-   client.send_animation(message.chat.id, "CgADBQADhQADwMtgVaLqemJ5PxrUAg" )
+   client.send_animation(message.chat.id, "CgADBQADhQADwMtgVWRwIy36e3OMAg" )
   if x == "2":
-   client.send_animation(message.chat.id, "CgADBQADhwADwMtgVQaNytQs6QzkAg" )
+   client.send_animation(message.chat.id, "CgADBQADhwADwMtgVShMwAN6RYE9Ag" )
   if x == "3":
-   client.send_animation(message.chat.id, "CgADBQADiQADwMtgVem_hevgaJBKAg" )
+   client.send_animation(message.chat.id, "CgADBQADiQADwMtgVS_IezDya4RqAg" )
   if x == "4":
-   client.send_animation(message.chat.id, "CgADBQADiwADwMtgVdiYv8C-xcAYAg" )
+   client.send_animation(message.chat.id, "CgADBQADiwADwMtgVXc_YnMpOr_sAg" )
   if x == "5":
-   client.send_animation(message.chat.id, "CgADBQADjQADwMtgVboSDeRCXzkmAg" )
+   client.send_animation(message.chat.id, "CgADBQADjQADwMtgVQzNN4NaRqMRAg" )
   if x == "6":
-   client.send_animation(message.chat.id, "CgADBQADjgADwMtgVbOcRpXM-lJXAg" )
+   client.send_animation(message.chat.id, "CgADBQADjgADwMtgVaqwp-zDO9txAg" )
   if x == "7":
-   client.send_animation(message.chat.id, "CgADBQADkwADwMtgVUfS6FOic5P2Ag" )
+   client.send_animation(message.chat.id, "CgADBQADkwADwMtgVTJA1Z0DuJ94Ag" )
   if x == "8":
-   client.send_animation(message.chat.id, "CgADBQADlgADwMtgVY0FXdi4xSaEAg" )
+   client.send_animation(message.chat.id, "CgADBQADlgADwMtgVWyiWDCNw4aGAg" )
   if x == "9":
-   client.send_animation(message.chat.id, "CgADBQADmgADwMtgVQ7JpGpzBaOOAg" )
+   client.send_animation(message.chat.id, "CgADBQADmgADwMtgVZ1EtpxPyvhHAg" )
   if x == "10":
-   client.send_animation(message.chat.id, "CgADBQADmQADwMtgVQNxdL6L1sYGAg" )
+   client.send_animation(message.chat.id, "CgADBQADmQADwMtgVU0_spSxU12_Ag" )
 
 
 @app.on_message(Filters.command('rolls'))
