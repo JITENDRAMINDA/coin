226b2fff51ae835453bd220336fdff8244b302f9
TSpidermanBoss <49564169+TSpidermanBoss@users.noreply.github.com>
2019-10-01 21:51:05 +0530

    Update tel.py

--- tel.py
+++ tel.py
@@ -1,3 +1,10 @@
+from telethon import TelegramClient
+
+client = TelegramClient("my",1134067,"f743379074210a6bf6830f77b4ba04ab")
+
 @client.on(events.NewMessage(pattern=r'^/start'))
 async def start_respond(e):
  await e.reply('/stort')
+
+with client:
+    client.loop.run_until_complete(main())
