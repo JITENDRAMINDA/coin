247b27c3e429993c62b938fdd2638aec6c6a155b
TSpidermanBoss <49564169+TSpidermanBoss@users.noreply.github.com>
2019-10-08 08:47:48 +0530

    Update dt.py

--- dt.py
+++ dt.py
@@ -2,7 +2,7 @@ from pyrogram import Client, Filters, Emoji
 import random
 import time
 
-app = Client("session",bot_token="633344746:AAFZpXfgLxbK-kZ4Z0zkQ4I8CJ9WlE0solg",api_id=605563,api_hash="7f2c2d12880400b88764b9b304e14e0b") 
+app = Client("session",bot_token="981972366:AAEvUMrd-CwuZRbjzD6zY9ycnJjah3OiCdc",api_id=605563,api_hash="7f2c2d12880400b88764b9b304e14e0b") 
 
 @app.on_message(Filters.command('rolls'))
 def ran(client, message):
