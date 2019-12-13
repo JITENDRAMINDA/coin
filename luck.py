28114adce29fe52657ce00e89efc372f69272a02
TSpidermanBoss <49564169+TSpidermanBoss@users.noreply.github.com>
2019-12-13 17:36:44 +0530

    Update luck.py

--- luck.py
+++ luck.py
@@ -2,7 +2,7 @@ from pyrogram import Client, Filters, Emoji
 import random
 import time
 
-bot = "953819366:AAF8EBTg0zuMgcBEk84FSvscNZXWjJ3N5jQ"
+bot = "668734257:AAFcWHnAbjcu_Or792LlRHiwx-FFAoNl2kI"
 
 app = Client("session",bot_token=bot,api_id=605563,api_hash="7f2c2d12880400b88764b9b304e14e0b") 
 @app.on_message(Filters. command('toss'))
